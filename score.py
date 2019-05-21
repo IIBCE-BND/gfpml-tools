c282160310f5d9f6e54756f9591d179bb10f3772
diego-silvera <diegoas1103@gmail.com>
2019-05-21 16:54:28 -0300

    initial commit

--- score.py
+++ score.py
@@ -8,7 +8,6 @@ from joblib import Parallel, delayed
 from parsers import parse_gtf, parse_obo, parse_annot
 
 from sklearn.model_selection import train_test_split
-from sklearn.metrics.pairwise import euclidean_distances
 from sklearn.metrics import pairwise_distances
 
 def score_function(num_genes_in_chromosome, positions):
@@ -36,39 +35,17 @@ def calculate_score(seq_genes, seq_annots, save_path, th=100):
 
     chromosome = seq_genes.seqname.values[0]
 
-    seg_genes = seq_genes.sort_values(['start', 'strand', 'size'], ascending=True) # correct order
+    seg_genes = seq_genes.sort_values(['start', 'strand', 'size'], ascending=True)
     seq_genes['pos'] = range(len(seq_genes))
     seq_len = len(seq_genes)
 
-    seq_lea = {}
+    seq_score = {}
     for go_id, seq_annots_go in seq_annots.groupby('go_id'):
-        if len(seq_annots_go) < th:
-            continue
-
         seq_annots_go['pos'] = seq_annots_go['gene_id'].replace(seq_genes.set_index(gene_identifier)['pos'].to_dict())
+        seq_score[go_id] = score_function(seq_len, seq_annots_go.pos.values)
 
-        train_size = 0.8
-        X_train, X_test, pos_train, pos_test = train_test_split(seq_annots_go.gene_id.values, seq_annots_go.pos.values, train_size=train_size)
-
-        train_data = {'pos':pos_train, 'gene_id':X_train}
-        train_df = pd.DataFrame(data=train_data)
-        test_data = {'pos':pos_test, 'gene_id':X_test}
-        test_df = pd.DataFrame(data=test_data)
-
-        if not os.path.exists('{}'.format(save_path)):
-            os.mkdir('{}'.format(save_path))
-        if not os.path.exists('{}/{}'.format(save_path, chromosome)):
-            os.mkdir('{}/{}'.format(save_path, chromosome))
-
-        train_df.to_csv('{}/{}/{}_train.csv'.format(save_path, chromosome, go_id), sep='\t', index=False)
-        test_df.to_csv('{}/{}/{}_test.csv'.format(save_path, chromosome, go_id), sep='\t', index=False)
-
-        mask_train = np.isin(range(seq_len), pos_train)
-        seq_lea[go_id] = calculate_enrichment(mask_train, window_size)
-
-    if len(seq_lea) > 0:
-        seq_lea = pd.DataFrame(data=seq_lea)
-        seq_lea.to_csv('{}/{}/seq_lea.csv'.format(save_path, chromosome), sep='\t', index=False)
+    seq_score = pd.DataFrame(data=seq_score)
+    seq_score.to_csv('{}/{}/seq_score.csv'.format(save_path, chromosome), sep='\t', index=False)
 
 
 @click.group()
